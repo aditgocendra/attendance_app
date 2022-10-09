6892ef3bb9d4424955be82e102b16f0dcc274a10
aditgocendra <gocendra123@gmail.com>
2022-10-09 10:11:32 +0700

    add haversine method

---
+++ app/src/main/java/com/ark/attendanceapp/DistanceMath/Haversine.java
@@ -0,0 +1,35 @@
+package com.ark.attendanceapp.DistanceMath;
+
+
+import android.util.Log;
+
+public class Haversine {
+    // haversine method
+    public static double haversine(double latUserLocation, double longUserLocation, double latSchoolLocation, double longSchoolLocation){
+        double distance;
+
+        // ---- /_\ lat and long
+        double triLat = Math.abs(latSchoolLocation - latUserLocation);
+        double triLong = Math.abs(longSchoolLocation - longUserLocation);
+
+        Log.d("tri", triLat +" "+ triLong);
+        // a = sin^2(/_\lat / 2) + cos(lat1).cos(lat2).sin^2(/_\long / 2)
+        double a = Math.pow(Math.sin(Math.toRadians(triLat / 2)), 2) +
+                Math.cos(Math.toRadians(latSchoolLocation)) * Math.cos(Math.toRadians(latUserLocation)) * Math.pow(Math.toRadians(Math.sin(triLong / 2)), 2);
+        Log.d("a", String.valueOf(a));
+
+        // arcSin = 2 * a sin sqrt(a)
+        double arcSin = 2 * Math.asin(Math.sqrt(a));
+        Log.d("arcSin", String.valueOf(arcSin));
+
+        // distance = R * arcSin
+        // R = 6371 (KM)
+        distance = 6371 * arcSin;
+        Log.d("distance", String.valueOf(distance));
+        // return distance
+        return distance;
+    }
+
+}
+
+
